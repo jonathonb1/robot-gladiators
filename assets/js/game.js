9b55d2b5959a15ab8217f6184bea7a5e969ea20e
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-07-14 13:00:06 -0700

    additional functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert ("This is an aler! JavaScript is running!");
+
+var playerName = window.prompt("What is your robot's name?");
+// note the lack of quoation marks around the playerName
+console.log(playerName);
+
+console.log("This logas a string, good for leaving yourself a message");
+// This will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robots name is " + playerName);
+
+
+
+// This creates a function named "fight"
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+
+// fight();
